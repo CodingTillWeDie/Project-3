e7535bae64a514e28722d547c2eedcc2e4f160e8
kevink0908 <98494454+kevink0908@users.noreply.github.com>
2022-03-29 19:27:08 -0700

    Update BinaryNode.java

--- BinaryNode.java
+++ BinaryNode.java
@@ -1,134 +1,104 @@
 class BinaryNode<T>
 {
     private T data;
-    private BinaryNode<T> leftChild;  // Reference to left child
-    private BinaryNode<T> rightChild; // Reference to right child
+    private BinaryNode<T> leftChild;  // Reference to left child.
+    private BinaryNode<T> rightChild; // Reference to right child.
 
     public BinaryNode()
     {
-        this(null); // Call next constructor
-    } // end default constructor
+        this(null); // Call next constructor.
+    } // end of default constructor
 
     public BinaryNode(T dataPortion)
     {
         this(dataPortion, null, null); // Call next constructor
-    } // end constructor
+    } // end of type constructor #1
 
-    public BinaryNode(T dataPortion, BinaryNode<T> newLeftChild,
-                      BinaryNode<T> newRightChild)
+    public BinaryNode(T dataPortion, BinaryNode<T> newLeftChild
+                                   , BinaryNode<T> newRightChild)
     {
         data = dataPortion;
         leftChild = newLeftChild;
         rightChild = newRightChild;
-    } // end constructor
+    } // end of type constructor #2
 
-    /**
-     * Retrieves the data portion of this node.
-     *
-     * @return The object in the data portion of the node.
-     */
+    /** Retrieves the data portion of this node.
+     * @return The object in the data portion of the node. */
     public T getData()
     {
         return data;
-    } // end getData
+    } // end of getData
 
-    /**
-     * Sets the data portion of this node.
-     *
-     * @param newData The data object.
-     */
+    /** Sets the data portion of this node.
+     * @param newData The data object. */
     public void setData(T newData)
     {
-        data = newData;
-    } // end setData
+        this.data = newData;
+    } // end of setData
 
-    /**
-     * Retrieves the left child of this node.
-     *
-     * @return A reference to this node's left child.
-     */
+    /** Retrieves the left child of this node.
+     * @return A reference to this node's left child. */
     public BinaryNode<T> getLeftChild()
     {
         return leftChild;
-    } // end getLeftChild
+    } // end of getLeftChild
 
-    /**
-     * Sets this node’s left child to a given node.
-     *
-     * @param newLeftChild A node that will be the left child.
-     */
+    /** Sets this node’s left child to a given node.
+     * @param newLeftChild A node that will be the left child. */
     public void setLeftChild(BinaryNode<T> newLeftChild)
     {
-        leftChild = newLeftChild;
-    } // end setLeftChild
+        this.leftChild = newLeftChild;
+    } // end of setLeftChild
 
-    /**
-     * Detects whether this node has a left child.
-     *
-     * @return True if the node has a left child.
-     */
+    /** Detects whether this node has a left child.
+     * @return True if the node has a left child. */
     public boolean hasLeftChild()
     {
         return leftChild != null;
-    } // end hasLeftChild
+    } // end of hasLeftChild
 
-    /**
-     * Retrieves the right child of this node.
-     *
-     * @return A reference to this node's right child.
-     */
+    /** Retrieves the right child of this node.
+     * @return A reference to this node's right child. */
     public BinaryNode<T> getRightChild()
     {
         return rightChild;
-    } // end getRightChild
+    } // end of getRightChild
 
-    /**
-     * Sets this node’s right child to a given node.
-     *
-     * @param newRightChild A node that will be the right child.
-     */
+    /** Sets this node’s right child to a given node.
+     * @param newRightChild A node that will be the right child. */
     public void setRightChild(BinaryNode<T> newRightChild)
     {
-        rightChild = newRightChild;
-    } // end setRightChild
+        this.rightChild = newRightChild;
+    } // end of setRightChild
 
-    /**
-     * Detects whether this node has a right child.
-     *
-     * @return True if the node has a right child.
-     */
+    /** Detects whether this node has a right child.
+     * @return True if the node has a right child. */
     public boolean hasRightChild()
     {
         return rightChild != null;
-    } // end hasRightChild
+    } // end of hasRightChild
 
-    /**
-     * Detects whether this node is a leaf.
-     *
-     * @return True if the node is a leaf.
-     */
+    /** Detects whether this node is a leaf.
+     * @return True if the node is a leaf. */
     public boolean isLeaf()
     {
         return (leftChild == null) && (rightChild == null);
-    } // end isLeaf
+    } // end of isLeaf
 
-    /**
-     * A Recursion Example in the BinaryNode Class
+    /** A Recursion Example in the BinaryNode Class
      * Copies the subtree rooted at this node.
-     *
-     * @return The root of a copy of the subtree rooted at this node.
-     */
+     * @return The root of a copy of the subtree rooted at this node. */
     public BinaryNode<T> copy()
     {
         BinaryNode<T> newRoot = new BinaryNode<>(data);
+
         if (leftChild != null)
             newRoot.setLeftChild(leftChild.copy());
-
         if (rightChild != null)
             newRoot.setRightChild(rightChild.copy());
 
         return newRoot;
-    } // end copy
+    } // end of copy
 
     /** --------------------------------------------------------------------
      * Part of Task 1 */
@@ -138,7 +108,8 @@ class BinaryNode<T>
      */
     public void postorderTraverse_binaryNodeMethod()
     {
-    }
+
+    } // end of postorderTraverse
 
     /**--------------------------------------------------------------------
      * Part of Task 2*/
@@ -151,24 +122,24 @@ class BinaryNode<T>
     public int getHeight_binaryNodeMethod()
     {
         return 0;
-    } // end getHeight
+    } // end of getHeight
 
     /** -------------------------------------------------------------------- */
-    /**
-     * A Recursive Method in the BinaryNode Class
+
+    /** A Recursive Method in the BinaryNode Class
      * Counts the nodes in the subtree rooted at "this" node.
-     *
-     * @return The number of nodes in the subtree rooted at "this" node.
-     */
+     * @return The number of nodes in the subtree rooted at "this" node. */
     public int getNumberOfNodes_binaryNodeMethod()
     {
         int leftNumber = 0;
         int rightNumber = 0;
+
         if (leftChild != null)
             leftNumber = leftChild.getNumberOfNodes_binaryNodeMethod();
         if (rightChild != null)
             rightNumber = rightChild.getNumberOfNodes_binaryNodeMethod();
-        return 1 + leftNumber + rightNumber;
-    } // end getNumberOfNodes
 
-} // end BinaryNode
+        return 1 + leftNumber + rightNumber;
+    } // end of getNumberOfNodes
+
+} // end of "BinaryNode" class
