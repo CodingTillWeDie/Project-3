9cb2408a5dd1291b42dc764250f85cfd1bd65329
Sean Abi Saab <98626715+seanabisaab@users.noreply.github.com>
2022-03-30 16:11:06 -0700

    Create BinaryTreeTest.java

---
+++ BinaryTreeTest.java
@@ -0,0 +1,21 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryTreeTest {
+
+    @Test
+    void postorderTraverse() {
+    }
+
+    @Test
+    void postorderTraverse_callBinaryNodeMethod() {
+    }
+
+    @Test
+    void getHeight_callBinaryNodeMethod() {
+    }
+
+    @Test
+    void getNumberOfNodes() {
+    }
+}
