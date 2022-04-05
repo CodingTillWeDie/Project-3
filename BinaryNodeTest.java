d9629ddb52f80e5b64bc53e1958e0302218743d9
Sean Abi Saab <98626715+seanabisaab@users.noreply.github.com>
2022-04-05 16:47:03 -0700

    Update BinaryNodeTest.java
    i dont think this is right

--- BinaryNodeTest.java
+++ BinaryNodeTest.java
@@ -1,14 +1,46 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class BinaryNodeTest {
 
     @Test
-    void postorderTraverse_binaryNodeMethod() {
+    public void postorderTraverse_binaryNodeMethod() {
+        BinaryTree<String> aTree = new BinaryTree<>();
+        createTree1(aTree);
+        System.out.println("(binaryNode) post-order: ");
+        aTree.postorderTraverse_callBinaryNodeMethod();
+
     }
 
     @Test
-    void getHeight_binaryNodeMethod() {
+    public int getHeight_binaryNodeMethod() {
+
+        BinaryTree<String> aTree = new BinaryTree<>();
+        createTree1(aTree);
+        System.out.println("(BinaryNode) Height of tree is " + aTree.getHeight_callBinaryNodeMethod());
+        return 0;
     }
+    public static void createTree1(BinaryTree<String> tree)
+    {
+        // Leaves
+        BinaryTree<String> dTree = new BinaryTree<>("D");
+        BinaryTree<String> eTree = new BinaryTree<>("E");
+        BinaryTree<String> gTree = new BinaryTree<>("G");
+
+        // Subtrees:
+        BinaryTree<String> fTree = new BinaryTree<>("F", null, gTree);
+        BinaryTree<String> bTree = new BinaryTree<>("B", dTree, eTree);
+        BinaryTree<String> cTree = new BinaryTree<>("C", fTree, null);
+
+        tree.setTree("A", bTree, cTree);
+
+        System.out.println("\nGiven Tree:\n");
+        System.out.println("     A      ");
+        System.out.println("   /   \\  ");
+        System.out.println("  B     C  ");
+        System.out.println(" / \\   /  ");
+        System.out.println("D   E  F   ");
+        System.out.println("        \\ ");
+        System.out.println("         G ");
+        System.out.println();
+    } // end createTree
 }
