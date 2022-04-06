89a99d19637413c51506ef61e3490b5924db2472
Sean Abi Saab <98626715+seanabisaab@users.noreply.github.com>
2022-04-05 22:56:43 -0700

    Delete type-search-index.js

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BinaryTree"},{"p":"<Unnamed>","l":"BinaryTreeInterface"},{"p":"<Unnamed>","l":"DriverBT"},{"p":"<Unnamed>","l":"EmptyTreeException"},{"p":"<Unnamed>","l":"TreeInterface"}];updateSearchResults();
